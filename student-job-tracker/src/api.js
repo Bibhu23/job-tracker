243b67f66bf149e0306c7ad4780a68aff60f4375
Bibhu23 <131533164+Bibhu23@users.noreply.github.com>
2025-04-10 21:08:13 +0530

    Update api.js

--- student-job-tracker/src/api.js
+++ student-job-tracker/src/api.js
@@ -1,10 +1,9 @@
-// src/api.js
+
 import axios from 'axios';
 
-// Dynamically set the base URL based on the environment
 const baseURL = process.env.NODE_ENV === 'production'
-    ? 'https://job-tracker-8.onrender.com/api/jobs'  // Production URL
-    : 'http://localhost:5000/api/jobs';               // Local development URL
+    ? 'https://job-tracker-8.onrender.com/api/jobs'  
+    : 'http://localhost:5000/api/jobs';              
 
 const API = axios.create({
     baseURL: baseURL,
