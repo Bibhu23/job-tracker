b745c7e5d879c9aa6fdc97bcd25a02b97024dbec
Bibhu23 <131533164+Bibhu23@users.noreply.github.com>
2025-04-10 21:03:13 +0530

    Update Jobs.js

--- backend/routes/Jobs.js
+++ backend/routes/Jobs.js
@@ -4,7 +4,7 @@ const Job = require("../models/Job");
 
 // POST: Add new job
 router.post("/", async (req, res) => {
-    console.log("📨 POST /api/jobs reached:", req.body); // Debug log
+    console.log("📨 POST /api/jobs reached:", req.body); 
     try {
         const newJob = new Job(req.body);
         const savedJob = await newJob.save();
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
-
+    // UPDATE: Update a job
 router.patch("/:id", async (req, res) => {
     try {
         const updated = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
